c505c0b03bbee9a406e8d62a7e76c4b7adab153a
Antanas Domarkas <curonia@gmail.com>
2017-02-13 20:35:43 +0200

    Adding initial modified version

--- config.h
+++ config.h
@@ -5,17 +5,22 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* UI Fonts */
+static const char *fonts[]          = {
+/*"monospace:size=10",*/
+"Meslo LG S:size=8:Style=Regular"
+};
+static const char dmenufont[]       = /*"monospace:size=10"*/"Meslo LG S:size=8:Style=Regular";
+static const char normbordercolor[] = "#073642";
+static const char normbgcolor[]	    = "#002B36";
+static const char normfgcolor[]     = "#839496";
+static const char selbordercolor[]  = "#586E75";
+static const char selbgcolor[]      = "#073642";
+static const char selfgcolor[]      = "#839496";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel] =  { selfgcolor, selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -56,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
